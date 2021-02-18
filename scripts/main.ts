b3601e331dc32c671f336a566e7ec55d9a4c6de5
DESKTOP-CTK4VV8\ftred_dev08 <hj.lee@fromthered.com>
2021-02-18 12:48:50 +0900

    test1

--- scripts/main.ts
+++ scripts/main.ts
@@ -8,7 +8,7 @@ window.onload = async function () {
 
     await ads.Instance.initialize( adContainer,
 
-        'https://googleads.g.doubleclick.net/pagead/ads?client=ca-video-afvtest&ad_type=video&description_url=http%3A%2F%2Fmyurl.com%2Fpage%3Fkey%3Dvalue%26key2%3Dvalue2'
+        'http://pubads.g.doubleclick.net/gampad/ads?sz=640x480&iu=/124319096/external/ad_rule_samples&ciu_szs=300x250&ad_rule=1&impl=s&gdfp_req=1&env=vp&output=xml_vmap1&unviewed_position_start=1&cust_params=sample_ar%3Dpremidpostpod%26deployment%3Dgmf-js&cmsid=496&vid=short_onecue&correlator='
         );
 
     button.addEventListener('click', ()=>{
